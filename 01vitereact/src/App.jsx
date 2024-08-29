c079e976c3cc6fb06a55dbd2ba86006896bbee7b
sahilchaudhari <sahilychaudhari@gmail.com>
2024-08-29 12:55:53 +0530

    Initial commit

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1>i am sahil</h1>
+    <h2>counter value:1</h2>
+    
+    </>
+  )
+}
+
+export default App
