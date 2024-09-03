9c00c41fdaa0c9d6193edfd83fc8dfea5cecbac1
sahilchaudhari <sahilychaudhari@gmail.com>
2024-09-03 12:57:48 +0530

    sasa
    sas

--- practice/src/index.js
+++ practice/src/index.js
@@ -2,34 +2,27 @@ import React from'react';
 import ReactDOM from 'react-dom';
 import"./index.css"
 
-const name="sahil";
-const img1="https://picsum.photos/200/300";
-const img2="https://picsum.photos/200/300";
-const img3="https://picsum.photos/200/300"
-const link="https://www.samsung.com/in/smartphones/galaxy-z-fold6/buy/?srsltid=AfmBOoptlk7EV8eHFGzRE2vyxdacWFVrztu0QilEmmRcuarc4Cd9jqNT"
+const name="i am sahil";
 
+
+const sahil={color:"red",textAlign:"center",}
 
 
 
 ReactDOM.render(
   <>
-  <h1 className='sahil'>
-    my name is {name}
+<h1 style={sahil}>{name}</h1>,
 
 
-  </h1>,
-  <div className='pankaj'>
-  <img src={img1}/>;
-  <a href={link} target='_blank'>
-  <img src={img2}/>;
-  </a>
-  <img src={img3}/>;
-  </div>
+<input type='text'placeholder='type here'/>,
+
 
  
 
 
-  {/* name:<input type="text" alt='this is haunted place'/> */}
+ 
+
+
 
 
   </>,
