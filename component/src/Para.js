7d2ad254639473b42f16d1a4b279f4e0ad9b4803
sahilchaudhari <sahilychaudhari@gmail.com>
2024-09-04 17:47:42 +0530

    update
    update

---
+++ component/src/Para.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Para(){
+    return <p>My Position Is</p>
+}
+
+export default Para;
---
+++ component/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Heading from './Heading';
+import Para from './Para';
+import Order from './Order';
+
+
+ReactDOM.render(
+  <>
+<Heading/>
+<Para/>
+<Order/>
+
+
+
+
+  
+  </>,
+  document.getElementById('root')
+);
